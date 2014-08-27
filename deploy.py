6ea03ba79697d101bf63a7a334fc5b47237a1f24
ekmartin <ekmartinb@gmail.com>
2014-08-28 01:03:12 +0200

    Further improvements to the python deploy script, better error handling and allow the config to determine the user

--- deploy.py
+++ deploy.py
@@ -1,30 +1,40 @@
-import sys, json
+#!/usr/bin/env python
+
+"""
+Takes a project name as the first argument and a git-branch as the second (optional).
+Finds the hostname from a config file, then uses ssh to deploy the project.
+"""
+
+import sys
+import json
 from fabric.api import env, run, cd
 
 
 class MissingProjectNameError(Exception):
-    pass
+    def __init__(self):
+        Exception.__init__(self, 'Missing project name argument.')
 
 
 class MissingServerError(Exception):
-    pass
+    def __init__(self, project_name):
+        Exception.__init__(self, 'Cant\'t find a server with the project name %s' % project_name)
 
 
-conf_file = 'servers.json'
-env.user = 'root'
+conf_file = 'servers.json'  # This could be an environment variable
+
 
 def get_host(project_name):
     with open(conf_file) as f:
-        servers = json.load(f, 'utf-8')
-        print servers
+        servers = json.load(f)
         for hostname, projects in servers.iteritems():
             if project_name in projects:
                 return hostname, projects[project_name]
-        raise MissingServerError
+        raise MissingServerError(project_name)
 
 
 def deploy_project(project_name, branch='master'):
     env.host_string, meta_info = get_host(project_name)
+    env.user = meta_info.get('  ') or 'root'
 
     with cd(meta_info['path']):
         run('git fetch && git reset --hard origin/%s' % branch)
@@ -32,7 +42,10 @@ def deploy_project(project_name, branch='master'):
 
 
 if __name__ == '__main__':
-    if len(sys.argv):
-        deploy_project(*sys.argv[1:])
-    else:
-        raise MissingProjectNameError
+    try:
+        if len(sys.argv):
+            deploy_project(*sys.argv[1:])
+        else:
+            raise MissingProjectNameError
+    except Exception as e:
+        sys.stderr.write(e.message)
