188080b344dbbc1a32b601009051ad2e6d0c39cc
Rolf Erik Lekang <me@rolflekang.com>
2015-02-18 19:20:06 +0100

    Fix #45, support custom passport strategy

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 require('colors');
 
-try {
   var chewie = require('./dist/index');
+try {
   module.export = chewie;
-} catch(e) {
+} catch (e) {
   if (e.code == 'MODULE_NOT_FOUND') {
     console.log('You need to run make parse'.red);
   } else {
-    throw e; 
+    throw e;
   }
 }
